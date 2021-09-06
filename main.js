b8c2f77041b16070e9780b7fc061f2c1e5fd06dc
Paul Abrams <john.paul.abrams@gmail.com>
2021-09-05 22:07:45 -0400

    git commit -sam feat: show image

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+const fs = require('fs')
 const path = require('path')
 
 function createWindow () {
@@ -7,22 +8,18 @@ function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
+    transparent: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.loadFile('index.html');
+  mainWindow.show();
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
+
   createWindow()
 
   app.on('activate', function () {
@@ -30,14 +27,15 @@ app.whenReady().then(() => {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+// app.on('window-all-closed', function () {
+//   if (process.platform !== 'darwin') electron.quit()
+// })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- preload.js
+++ preload.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
@@ -9,4 +11,14 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
+
+  const images = new Array();
+  for (let i=0; i< 10; i++){
+    images[i] = `${__dirname}/layer${i}-temp.png`;
+  }
+
+  var _img = fs.readFileSync(images[0]).toString('base64');
+  var img = new Image();
+  img.src = images[0];
+  document.body.appendChild(img);
 })
